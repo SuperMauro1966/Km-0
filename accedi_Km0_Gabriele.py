69f5c7425e2a447289896371bdedcadc5cc71e13
tazzi <gabrieleale2106@gmail.com>
2024-05-22 15:46:50 +0200

    file per accedere

---
+++ accedi_Km0_Gabriele.py
@@ -0,0 +1,34 @@
+import sys
+
+import mariadb
+
+try:
+    conn=mariadb.connect(
+        user="root",
+        password="1234",
+        host="127.0.0.1",
+        port=3306,
+        database="km-0"
+    )
+
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB platform: {e}")
+    sys.exit(1)
+
+
+email=input("Inserisci l'e-mail: ")
+pwd=input("Inserisci la password: ")
+
+cur=conn.cursor()
+cur.execute(
+    f"SELECT COUNT(*) FROM account WHERE email = '{email}' AND password = '{pwd}';")
+
+count = cur.fetchone()[0]
+conn.commit()
+
+# Verifica se c'è almeno una corrispondenza
+if count==1:
+    print("Accesso consentito.")
+else:
+    print("Nome utente o password errati.")
+
