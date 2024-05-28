279bd508387c0f5aa75c52418d9e454b9f5d7aee
tazzi <gabrieleale2106@gmail.com>
2024-05-28 11:20:34 +0200

    Aggiunta tabella venditore

--- sql/km-0.sql
+++ sql/km-0.sql
@@ -20,20 +20,37 @@ DROP DATABASE IF EXISTS `km-0`;
 CREATE DATABASE IF NOT EXISTS `km-0` /*!40100 DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci */;
 USE `km-0`;
 
--- Dump della struttura di tabella km-0.credential
-DROP TABLE IF EXISTS `credential`;
-CREATE TABLE IF NOT EXISTS `credential` (
+-- Dump della struttura di tabella km-0.tbcredential
+DROP TABLE IF EXISTS `tbcredential`;
+CREATE TABLE IF NOT EXISTS `tbcredential` (
   `idCredential` int(11) NOT NULL AUTO_INCREMENT,
   `pswd` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`idCredential`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Dump dei dati della tabella km-0.credential: ~2 rows (circa)
-DELETE FROM `credential`;
-INSERT INTO `credential` (`idCredential`, `pswd`, `email`) VALUES
+-- Dump dei dati della tabella km-0.tbcredential: ~2 rows (circa)
+DELETE FROM `tbcredential`;
+INSERT INTO `tbcredential` (`idCredential`, `pswd`, `email`) VALUES
 	(1, '1234', 'Sergio'),
 	(2, '1234', 'Gabriele');
+
+-- Dump della struttura di tabella km-0.tbvenditore
+DROP TABLE IF EXISTS `tbvenditore`;
+CREATE TABLE IF NOT EXISTS `tbvenditore` (
+  `idVenditore` int(11) NOT NULL AUTO_INCREMENT,
+  `sitoweb` varchar(255) DEFAULT NULL,
+  `partitaIVA` int(11) DEFAULT NULL,
+  `ragioneSociale` varchar(255) DEFAULT NULL,
+  `CF` varchar(16) DEFAULT NULL,
+  `idCredential` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idVenditore`),
+  KEY `codCredential` (`idCredential`) USING BTREE,
+  CONSTRAINT `tbvenditore_ibfk_1` FOREIGN KEY (`idCredential`) REFERENCES `tbcredential` (`idCredential`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Dump dei dati della tabella km-0.tbvenditore: ~0 rows (circa)
+DELETE FROM `tbvenditore`;
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
