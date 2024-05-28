fa63c1a9af65b1c5a6f95370b1376d1ed498ca4e
tazzi <gabrieleale2106@gmail.com>
2024-05-28 14:48:22 +0200

    Aggiunte le tabelle Pagamento, Ordine e Recensione

--- sql/km-0.sql
+++ sql/km-0.sql
@@ -1,9 +1,9 @@
--- ------------------------------------------------------
+-- --------------------------------------------------------
 -- Host:                         127.0.0.1
 -- Versione server:              10.6.18-MariaDB - mariadb.org binary distribution
 -- S.O. server:                  Win64
 -- HeidiSQL Versione:            12.6.0.6765
--- -------------------------------------------------------
+-- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
@@ -25,7 +25,7 @@ DROP TABLE IF EXISTS `tbcliente`;
 CREATE TABLE IF NOT EXISTS `tbcliente` (
   `idCliente` int(11) NOT NULL AUTO_INCREMENT,
   `citta` varchar(34) NOT NULL,
-  `provincia` varchar(2) NOT NULL,
+  `provincia` varchar(34) NOT NULL,
   `via` varchar(255) NOT NULL,
   `nome` varchar(255) NOT NULL,
   `cognome` varchar(255) NOT NULL,
@@ -35,75 +35,67 @@ CREATE TABLE IF NOT EXISTS `tbcliente` (
   PRIMARY KEY (`idCliente`),
   KEY `FK_tbcliente_idCredential_tbcredential_idCredential` (`idCredential`),
   CONSTRAINT `FK_tbcliente_idCredential_tbcredential_idCredential` FOREIGN KEY (`idCredential`) REFERENCES `tbcredential` (`idCredential`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Dump dei dati della tabella km-0.tbcliente: ~1 rows (circa)
+-- Dump dei dati della tabella km-0.tbcliente: ~0 rows (circa)
 DELETE FROM `tbcliente`;
-INSERT INTO `tbcliente` (`idCliente`, `citta`, `provincia`, `via`, `nome`, `cognome`, `CF`, `telefono`, `idCredential`) VALUES
-	(1, 'Imola', 'BO', 'Via della liberazione', 'Mario', 'Rossi', 'AAA', '+39', 3);
 
 -- Dump della struttura di tabella km-0.tbcredential
 DROP TABLE IF EXISTS `tbcredential`;
 CREATE TABLE IF NOT EXISTS `tbcredential` (
   `idCredential` int(11) NOT NULL AUTO_INCREMENT,
   `pswd` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`idCredential`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Dump dei dati della tabella km-0.tbcredential: ~4 rows (circa)
+-- Dump dei dati della tabella km-0.tbcredential: ~2 rows (circa)
 DELETE FROM `tbcredential`;
 INSERT INTO `tbcredential` (`idCredential`, `pswd`, `email`) VALUES
 	(1, '1234', 'Sergio'),
-	(2, '1234', 'Gabriele'),
-	(3, '5678', 'Pippo'),
-	(4, '5678', 'Pluto');
+	(2, '1234', 'Gabriele');
 
--- Dump della struttura di tabella km-0.tbevento
-DROP TABLE IF EXISTS `tbevento`;
-CREATE TABLE IF NOT EXISTS `tbevento` (
-  `idEvento` int(11) NOT NULL AUTO_INCREMENT,
-  `descrizione` text NOT NULL,
-  `dataInizio` datetime NOT NULL,
-  `dataFine` datetime NOT NULL,
-  `nome` varchar(255) NOT NULL,
-  `condiviso` tinyint(1) NOT NULL,
-  `idCredential` int(11) NOT NULL,
-  PRIMARY KEY (`idEvento`),
-  KEY `FK_tbevento_idCredential_tbcredential_idCredential` (`idCredential`) USING BTREE,
-  CONSTRAINT `FK_tbevento_idCredential_tbcredential_idCredential` FOREIGN KEY (`idCredential`) REFERENCES `tbcredential` (`idCredential`) ON DELETE CASCADE ON UPDATE CASCADE
+-- Dump della struttura di tabella km-0.tbordine
+DROP TABLE IF EXISTS `tbordine`;
+CREATE TABLE IF NOT EXISTS `tbordine` (
+  `idOrdine` int(11) NOT NULL AUTO_INCREMENT,
+  `stato` varchar(10) NOT NULL,
+  `data` datetime NOT NULL,
+  `idRecensione` int(11) NOT NULL,
+  PRIMARY KEY (`idOrdine`),
+  KEY `FK_tbordine_idRecensione_tbpagameneto_idPagamento` (`idRecensione`),
+  CONSTRAINT `FK_tbordine_idRecensione_tbpagameneto_idPagamento` FOREIGN KEY (`idRecensione`) REFERENCES `tbpagamento` (`idPagamento`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Dump dei dati della tabella km-0.tbevento: ~0 rows (circa)
-DELETE FROM `tbevento`;
+-- Dump dei dati della tabella km-0.tbordine: ~0 rows (circa)
+DELETE FROM `tbordine`;
 
--- Dump della struttura di tabella km-0.tbservizio
-DROP TABLE IF EXISTS `tbservizio`;
-CREATE TABLE IF NOT EXISTS `tbservizio` (
-  `idServizio` int(11) NOT NULL AUTO_INCREMENT,
+-- Dump della struttura di tabella km-0.tbpagamento
+DROP TABLE IF EXISTS `tbpagamento`;
+CREATE TABLE IF NOT EXISTS `tbpagamento` (
+  `idPagamento` int(11) NOT NULL AUTO_INCREMENT,
   `descrizione` text NOT NULL,
   `nome` varchar(255) NOT NULL,
-  PRIMARY KEY (`idServizio`)
+  PRIMARY KEY (`idPagamento`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Dump dei dati della tabella km-0.tbservizio: ~0 rows (circa)
-DELETE FROM `tbservizio`;
+-- Dump dei dati della tabella km-0.tbpagamento: ~0 rows (circa)
+DELETE FROM `tbpagamento`;
 
--- Dump della struttura di tabella km-0.tbubicazione
-DROP TABLE IF EXISTS `tbubicazione`;
-CREATE TABLE IF NOT EXISTS `tbubicazione` (
-  `idUbicazione` int(11) NOT NULL AUTO_INCREMENT,
-  `citta` varchar(255) NOT NULL,
-  `provincia` varchar(2) NOT NULL,
-  `via` varchar(255) NOT NULL,
-  `fissa` tinyint(1) NOT NULL,
-  `orario` text NOT NULL,
-  `attiva` tinyint(1) NOT NULL,
-  PRIMARY KEY (`idUbicazione`)
+-- Dump della struttura di tabella km-0.tbrecensione
+DROP TABLE IF EXISTS `tbrecensione`;
+CREATE TABLE IF NOT EXISTS `tbrecensione` (
+  `idRecensione` int(11) NOT NULL AUTO_INCREMENT,
+  `recensione` text NOT NULL,
+  `valutazione` int(11) NOT NULL,
+  `idCliente` int(11) NOT NULL,
+  PRIMARY KEY (`idRecensione`),
+  KEY `FK_tbrecensione_idCliente_tbcliente_idCliente` (`idCliente`),
+  CONSTRAINT `FK_tbrecensione_idCliente_tbcliente_idCliente` FOREIGN KEY (`idCliente`) REFERENCES `tbcliente` (`idCliente`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Dump dei dati della tabella km-0.tbubicazione: ~0 rows (circa)
-DELETE FROM `tbubicazione`;
+-- Dump dei dati della tabella km-0.tbrecensione: ~0 rows (circa)
+DELETE FROM `tbrecensione`;
 
 -- Dump della struttura di tabella km-0.tbvenditore
 DROP TABLE IF EXISTS `tbvenditore`;
@@ -115,27 +107,12 @@ CREATE TABLE IF NOT EXISTS `tbvenditore` (
   `CF` varchar(16) NOT NULL,
   `idCredential` int(11) NOT NULL,
   PRIMARY KEY (`idVenditore`),
-  KEY `FK_tbvenditore_idCredential_tbcredential_idCredential` (`idCredential`) USING BTREE,
-  CONSTRAINT `tbvenditore_ibfk_1` FOREIGN KEY (`idCredential`) REFERENCES `tbcredential` (`idCredential`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+  KEY `codCredential` (`idCredential`) USING BTREE,
+  CONSTRAINT `FK_tbvenditore_idCredential_tbcredential_idCredential` FOREIGN KEY (`idCredential`) REFERENCES `tbcredential` (`idCredential`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Dump dei dati della tabella km-0.tbvenditore: ~1 rows (circa)
+-- Dump dei dati della tabella km-0.tbvenditore: ~0 rows (circa)
 DELETE FROM `tbvenditore`;
-INSERT INTO `tbvenditore` (`idVenditore`, `sitoweb`, `partitaIVA`, `ragioneSociale`, `CF`, `idCredential`) VALUES
-	(1, 'www.disney.it', 10, 'Topolino', 'BBB', 4);
-
--- Dump della struttura di vista km-0.vwadmin
-DROP VIEW IF EXISTS `vwadmin`;
--- Creazione di una tabella temporanea per risolvere gli errori di dipendenza della vista
-CREATE TABLE `vwadmin` (
-	`idCredential` INT(11) NOT NULL,
-	`email` VARCHAR(255) NOT NULL COLLATE 'latin1_swedish_ci',
-	`pswd` VARCHAR(255) NOT NULL COLLATE 'latin1_swedish_ci'
-) ENGINE=MyISAM;
-
--- Rimozione temporanea di tabella e creazione della struttura finale della vista
-DROP TABLE IF EXISTS `vwadmin`;
-CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `vwadmin` AS select `tbcredential`.`idCredential` AS `idCredential`,`tbcredential`.`email` AS `email`,`tbcredential`.`pswd` AS `pswd` from `tbcredential` where !(`tbcredential`.`idCredential` in ((select `tbvenditore`.`idCredential` from `tbvenditore`) union (select `tbcliente`.`idCredential` from `tbcliente`)));
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
