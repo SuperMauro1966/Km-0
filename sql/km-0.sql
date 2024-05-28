1b1f623f44baf63fb602aaf21e0f486a4f28e829
SergioXYZ <sergiodebenedetto2006@gmail.com>
2024-05-28 15:41:55 +0200

    aggiunta delle ultime tabelle e modifica delle dimensioni del prezzo nel diagramma logico

--- sql/km-0.sql
+++ sql/km-0.sql
@@ -16,10 +16,23 @@
 
 
 -- Dump della struttura del database km-0
+DROP DATABASE IF EXISTS `km-0`;
 CREATE DATABASE IF NOT EXISTS `km-0` /*!40100 DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci */;
 USE `km-0`;
 
+-- Dump della struttura di tabella km-0.tbaccetta
+DROP TABLE IF EXISTS `tbaccetta`;
+CREATE TABLE IF NOT EXISTS `tbaccetta` (
+  `idVenditore` int(11) NOT NULL,
+  `idPagamento` int(11) NOT NULL,
+  PRIMARY KEY (`idVenditore`,`idPagamento`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Dump dei dati della tabella km-0.tbaccetta: ~0 rows (circa)
+DELETE FROM `tbaccetta`;
+
 -- Dump della struttura di tabella km-0.tbcliente
+DROP TABLE IF EXISTS `tbcliente`;
 CREATE TABLE IF NOT EXISTS `tbcliente` (
   `idCliente` int(11) NOT NULL AUTO_INCREMENT,
   `citta` varchar(34) NOT NULL,
@@ -38,7 +51,30 @@ CREATE TABLE IF NOT EXISTS `tbcliente` (
 -- Dump dei dati della tabella km-0.tbcliente: ~0 rows (circa)
 DELETE FROM `tbcliente`;
 
+-- Dump della struttura di tabella km-0.tbcontiene
+DROP TABLE IF EXISTS `tbcontiene`;
+CREATE TABLE IF NOT EXISTS `tbcontiene` (
+  `idServizio` int(11) NOT NULL,
+  `idOrdine` int(11) NOT NULL,
+  PRIMARY KEY (`idServizio`,`idOrdine`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Dump dei dati della tabella km-0.tbcontiene: ~0 rows (circa)
+DELETE FROM `tbcontiene`;
+
+-- Dump della struttura di tabella km-0.tbcrea
+DROP TABLE IF EXISTS `tbcrea`;
+CREATE TABLE IF NOT EXISTS `tbcrea` (
+  `idVenditore` int(11) NOT NULL,
+  `idEvento` int(11) NOT NULL,
+  PRIMARY KEY (`idVenditore`,`idEvento`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Dump dei dati della tabella km-0.tbcrea: ~0 rows (circa)
+DELETE FROM `tbcrea`;
+
 -- Dump della struttura di tabella km-0.tbcredential
+DROP TABLE IF EXISTS `tbcredential`;
 CREATE TABLE IF NOT EXISTS `tbcredential` (
   `idCredential` int(11) NOT NULL AUTO_INCREMENT,
   `pswd` varchar(255) NOT NULL,
@@ -52,7 +88,19 @@ INSERT INTO `tbcredential` (`idCredential`, `pswd`, `email`) VALUES
 	(1, '1234', 'Sergio'),
 	(2, '1234', 'Gabriele');
 
+-- Dump della struttura di tabella km-0.tbeffettua
+DROP TABLE IF EXISTS `tbeffettua`;
+CREATE TABLE IF NOT EXISTS `tbeffettua` (
+  `idCliente` int(11) NOT NULL,
+  `idOrdine` int(11) NOT NULL,
+  `idPagamento` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Dump dei dati della tabella km-0.tbeffettua: ~0 rows (circa)
+DELETE FROM `tbeffettua`;
+
 -- Dump della struttura di tabella km-0.tbevento
+DROP TABLE IF EXISTS `tbevento`;
 CREATE TABLE IF NOT EXISTS `tbevento` (
   `idEvento` int(11) NOT NULL AUTO_INCREMENT,
   `descrizione` text NOT NULL,
@@ -69,7 +117,32 @@ CREATE TABLE IF NOT EXISTS `tbevento` (
 -- Dump dei dati della tabella km-0.tbevento: ~0 rows (circa)
 DELETE FROM `tbevento`;
 
+-- Dump della struttura di tabella km-0.tbmodalitapagamento
+DROP TABLE IF EXISTS `tbmodalitapagamento`;
+CREATE TABLE IF NOT EXISTS `tbmodalitapagamento` (
+  `idPagamento` int(11) NOT NULL,
+  `idOrdine` int(11) NOT NULL,
+  PRIMARY KEY (`idPagamento`,`idOrdine`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Dump dei dati della tabella km-0.tbmodalitapagamento: ~0 rows (circa)
+DELETE FROM `tbmodalitapagamento`;
+
+-- Dump della struttura di tabella km-0.tboffre
+DROP TABLE IF EXISTS `tboffre`;
+CREATE TABLE IF NOT EXISTS `tboffre` (
+  `idPagamento` int(11) NOT NULL,
+  `idServizio` int(11) NOT NULL,
+  `prezzo` float NOT NULL,
+  `quantita` text NOT NULL,
+  PRIMARY KEY (`idPagamento`,`idServizio`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Dump dei dati della tabella km-0.tboffre: ~0 rows (circa)
+DELETE FROM `tboffre`;
+
 -- Dump della struttura di tabella km-0.tbordine
+DROP TABLE IF EXISTS `tbordine`;
 CREATE TABLE IF NOT EXISTS `tbordine` (
   `idOrdine` int(11) NOT NULL AUTO_INCREMENT,
   `stato` varchar(10) NOT NULL,
@@ -84,6 +157,7 @@ CREATE TABLE IF NOT EXISTS `tbordine` (
 DELETE FROM `tbordine`;
 
 -- Dump della struttura di tabella km-0.tbpagamento
+DROP TABLE IF EXISTS `tbpagamento`;
 CREATE TABLE IF NOT EXISTS `tbpagamento` (
   `idPagamento` int(11) NOT NULL AUTO_INCREMENT,
   `descrizione` text NOT NULL,
@@ -95,6 +169,7 @@ CREATE TABLE IF NOT EXISTS `tbpagamento` (
 DELETE FROM `tbpagamento`;
 
 -- Dump della struttura di tabella km-0.tbrecensione
+DROP TABLE IF EXISTS `tbrecensione`;
 CREATE TABLE IF NOT EXISTS `tbrecensione` (
   `idRecensione` int(11) NOT NULL AUTO_INCREMENT,
   `recensione` text NOT NULL,
@@ -109,6 +184,7 @@ CREATE TABLE IF NOT EXISTS `tbrecensione` (
 DELETE FROM `tbrecensione`;
 
 -- Dump della struttura di tabella km-0.tbservizio
+DROP TABLE IF EXISTS `tbservizio`;
 CREATE TABLE IF NOT EXISTS `tbservizio` (
   `idServizio` int(11) NOT NULL AUTO_INCREMENT,
   `descrizione` text NOT NULL,
@@ -119,7 +195,30 @@ CREATE TABLE IF NOT EXISTS `tbservizio` (
 -- Dump dei dati della tabella km-0.tbservizio: ~0 rows (circa)
 DELETE FROM `tbservizio`;
 
+-- Dump della struttura di tabella km-0.tbsirivolgea
+DROP TABLE IF EXISTS `tbsirivolgea`;
+CREATE TABLE IF NOT EXISTS `tbsirivolgea` (
+  `idServizio` int(11) NOT NULL,
+  `idOrdine` int(11) NOT NULL,
+  PRIMARY KEY (`idServizio`,`idOrdine`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Dump dei dati della tabella km-0.tbsirivolgea: ~0 rows (circa)
+DELETE FROM `tbsirivolgea`;
+
+-- Dump della struttura di tabella km-0.tbsistabiliscein
+DROP TABLE IF EXISTS `tbsistabiliscein`;
+CREATE TABLE IF NOT EXISTS `tbsistabiliscein` (
+  `idVenditore` int(11) NOT NULL,
+  `idUbicazione` int(11) NOT NULL,
+  PRIMARY KEY (`idVenditore`,`idUbicazione`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Dump dei dati della tabella km-0.tbsistabiliscein: ~0 rows (circa)
+DELETE FROM `tbsistabiliscein`;
+
 -- Dump della struttura di tabella km-0.tbubicazione
+DROP TABLE IF EXISTS `tbubicazione`;
 CREATE TABLE IF NOT EXISTS `tbubicazione` (
   `idUbicazione` int(11) NOT NULL AUTO_INCREMENT,
   `citta` varchar(255) NOT NULL,
@@ -135,6 +234,7 @@ CREATE TABLE IF NOT EXISTS `tbubicazione` (
 DELETE FROM `tbubicazione`;
 
 -- Dump della struttura di tabella km-0.tbvenditore
+DROP TABLE IF EXISTS `tbvenditore`;
 CREATE TABLE IF NOT EXISTS `tbvenditore` (
   `idVenditore` int(11) NOT NULL AUTO_INCREMENT,
   `sitoweb` varchar(255) NOT NULL,
@@ -151,6 +251,7 @@ CREATE TABLE IF NOT EXISTS `tbvenditore` (
 DELETE FROM `tbvenditore`;
 
 -- Dump della struttura di vista km-0.vwadmin
+DROP VIEW IF EXISTS `vwadmin`;
 -- Creazione di una tabella temporanea per risolvere gli errori di dipendenza della vista
 CREATE TABLE `vwadmin` (
 	`idCredential` INT(11) NOT NULL,
