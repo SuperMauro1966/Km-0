15b9d85d03537083658dffc302b403286e41b970
SergioXYZ <sergiodebenedetto2006@gmail.com>
2024-05-30 14:47:06 +0200

    modifica dell'algoritmo per la gestione delle registrazioni, aggiunta di una funzione che controlla se l'email è già stata inserita

--- src/main.py
+++ src/main.py
@@ -14,50 +14,55 @@ from  controller import accedi,db
     cur.close()
     return row==1
     """
-def registrati():
+def check_email(email):
+    cur=conn.cursor()
+    cur.execute(f"SELECT COUNT(idCredential) FROM tbcredential WHERE email='{email}'")
+    ch_mail=cur.fetchone()[0]
+    cur.close()
+    return ch_mail==0
+
+def registrati(email, password, codice_fiscale, telefono, ruolo):
     # inserimento dati x tbcredential
-    email=input("Inserisci l'e-mail: ")
-    cur.execute(f"SELECT COUNT(idCredential) FROM tbcredential WHERE email='{email}';")
-    check_email=cur.fetchone()[0]
-    if check_email==1:
-        print("è già stato creato un account con questa mail")
+    while ruolo!='C' and ruolo!='V' and ruolo!='c' and ruolo!='v':
+        ruolo=input("\nInserisci il ruolo: \nV o v per venditore\nC o c per cliente\n")
+
+    if ruolo=='C' or ruolo=='c':
+        first_name=input("\nInserisci il nome: ")
+        last_name=input("Inserisci il cognome: ")
+        via=input("Inserisci la via: ")    
+        citta=input("Inserisci la città: ")
+        provincia=input("Inserisci la provincia: ")
+
+        cur=conn.cursor()
+        cur.execute(f"INSERT INTO tbcredential (idCredential, email, pswd, attivo) VALUES (NULL, '{email}', '{password}', 1);")
+        conn.commit()
+        cur.close()
+
+        cur=conn.cursor()
+        cur.execute("SELECT idCredential FROM tbcredential WHERE email='{email}';")
+        idC=cur.fetchone()[0]
+        cur.close()
+
+        cur=conn.cursor()
+        cur.execute(f"INSERT INTO tbcliente (idCliente, citta, provincia, via, nome, cognome, CF, telefono, `idCredential) VALUES (NULL, '{citta}', '{provincia}', '{via}', '{first_name}', '{last_name}', '{codice_fiscale}', '{telefono}', {idC[0]});")
+        conn.commit()
+        cur.close()
+        print("Account registrato con successo.")
     else:
-        pwd=input("Inserisci la password: ")
-        confirm_pwd=input("Inserisci nuovamente la password: ")
-        while pwd!=confirm_pwd:
-            print("Le password non corrispondono")
-            confirm_pwd=input("Inserisci nuovamente la password: ")
-        #inserimento dei dati in comune
-        codice_fiscale=input("Inserisci il codice fiscale: ")
-        telefono=input("Inserisci il numero di telefono: ")
-        ruolo=input("Inserisci il ruolo: \nV o v per venditore\nC o c per cliente\n")
-        while ruolo!='C' and ruolo!='V' and ruolo!='c' and ruolo!='v':
-            ruolo=input("\nInserisci il ruolo: \nV o v per venditore\nC o c per cliente\n")
+        ragione_sociale=input("\nInserisci la ragione sociale: ")
+        sitoweb=input("Inserisci un eventuale sito web: ")
+        partitaIVA=int(input("Inserisci la partita IVA: "))
             
-        if ruolo=='C' or ruolo=='c':
-            first_name=input("\nInserisci il nome: ")
-            last_name=input("Inserisci il cognome: ")
-            via=input("Inserisci la via: ")    
-            citta=input("Inserisci la città: ")
-            provincia=input("Inserisci la provincia: ")
-            cur.execute(f"INSERT INTO tbcredential (idCredential, email, pswd, attivo) VALUES (NULL, '{email}', '{pwd}', 1);")
-            conn.commit()
-            cur.execute("SELECT idCredential FROM tbcredential;")
-            idC=cur.fetchone()[0]
-            cur.execute(f"INSERT INTO tbcliente (idCliente, citta, provincia, via, nome, cognome, CF, telefono, `idCredential) VALUES (NULL, '{citta}', '{provincia}', '{via}', '{first_name}', '{last_name}', '{codice_fiscale}', '{telefono}', {idC[0]});")
-            conn.commit()
-            print("Account registrato con successo.")
-        else:
-            ragione_sociale=input("\nInserisci la ragione sociale: ")
-            sitoweb=input("Inserisci un eventuale sito web: ")
-            partitaIVA=int(input("Inserisci la partita IVA: "))
-            
-            cur.execute(f"INSERT INTO tbcredential (idCredential, email, pswd, attivo) VALUES (NULL, '{email}', '{pwd}', 1);")
-            conn.commit()
-            cur.execute("SELECT idCredential FROM tbcredential;")
-            idC=cur.fetchone()[0]
-            cur.execute(f"INSERT INTO tbvenditore (idVenditore, sitoweb, partitaIVA, ragioneSociale, CF, telefono, idCredential) VALUES (NULL,'{sitoweb}', {partitaIVA}, '{ragione_sociale}', '{codice_fiscale}', '{telefono}', {idC[0]});")
-            conn.commit()
+        cur=conn.cursor()
+        cur.execute(f"INSERT INTO tbcredential (idCredential, email, pswd, attivo) VALUES (NULL, '{email}', '{password}', 1);")
+        conn.commit()
+        cur.close()
+
+        cur=conn.cursor()
+        cur.execute("SELECT idCredential FROM tbcredential WHERE email='{email}';")
+        idC=cur.fetchone()[0]
+        cur.execute(f"INSERT INTO tbvenditore (idVenditore, sitoweb, partitaIVA, ragioneSociale, CF, telefono, idCredential) VALUES (NULL,'{sitoweb}', {partitaIVA}, '{ragione_sociale}', '{codice_fiscale}', '{telefono}', {idC[0]});")
+        conn.commit()
 
 def main():
     try:
@@ -74,20 +79,35 @@ def main():
 
     #print(globals())
     while True:
-        scelta=int(input("\nScegli un'opzione: \n1. per accedere\n2. per registrarsi\n3. per uscire\n"))
+        scelta=int(input("\nScegli un'opzione:\n1. per accedere\n2. per registrarsi\n3. per uscire\n"))
         while scelta<1 or scelta>3:
-            scelta=int(input("\nScegli un'opzione: \n1. per accedere\n2. per registrarsi\n3. per uscire\n"))
+            scelta=int(input("\nScegli un'opzione:\n1. per accedere\n2. per registrarsi\n3. per uscire\n"))
 
         print("\n")
         if scelta==1:
-            email=input("digita l'email: ")
-            password=input("digita la password: ")
+            email=input("Digita l'email: ")
+            password=input("Digita la password: ")
             if accedi(email, password):
                 print("Autenticato correttamente!")
             else:
                 print("Errore nel login, l'utente potrebbe non esistere o disattivato dall'admin")
         elif scelta==2:
-            registrati()
+            email=input("Digita l'email: ")
+            if check_email(email):
+                print("Errore! Email già utilizzata")
+                break
+
+            #inserimento dei campi in comune
+            password=input("Inserisci la password: ")
+            confirm_pwd=input("Inserisci nuovamente la password: ")
+            while password!=confirm_pwd:
+                print("Le password non corrispondono")
+                confirm_pwd=input("Inserisci nuovamente la password: ")
+            codice_fiscale=input("Inserisci il codice fiscale: ")
+            telefono=input("Inserisci il numero di telefono: ")
+            
+            ruolo=input("Inserisci il ruolo: \nV o v per venditore\nC o c per cliente\n")
+            registrati(email, password, codice_fiscale, telefono, ruolo)
         else:
             conn.close()
             break
