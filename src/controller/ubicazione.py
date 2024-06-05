382affe0fb902431888ef5022b5a8b8c2a2dacd9
tazzi <gabrieleale2106@gmail.com>
2024-06-05 11:01:17 +0200

    Importato il DB

---
+++ src/controller/ubicazione.py
@@ -0,0 +1,11 @@
+from controller import db
+
+def inserimento_ubicazione():
+    
+    pass
+def modifica_ubicazione():
+    pass
+def elimina_ubicazione():
+    pass
+def mostra_ubicazione():
+    pass
