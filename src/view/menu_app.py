5e502b6feb67af870720374b69778d62dc0efc66
SergioXYZ <sergiodebenedetto2006@gmail.com>
2024-06-06 10:55:13 +0200

    tutte le variabili tranne manu_accesso_app sono private

--- src/view/menu_app.py
+++ src/view/menu_app.py
@@ -1,13 +1,13 @@
-from .menu import MenuItem, SubMenu, Cmd
+from .menu.menu import MenuItem, SubMenu, Cmd
 from ..controller.login import accedi
 from . import get_reg_dati, get_log_dati
 
-cmd_login = Cmd("Login", get_log_dati)
-cmd_registra = Cmd("Registrazione", get_reg_dati)
+_cmd_login = Cmd("Login", get_log_dati)
+_cmd_registra = Cmd("Registrazione", get_reg_dati)
 
-item_login = MenuItem("Login", cmd_login, [])
-item_registra = MenuItem("Registrazione", cmd_registra, [])
+_item_login = MenuItem("Login", _cmd_login, [])
+_item_registra = MenuItem("Registrazione", _cmd_registra, [])
 
 menu_accesso_app = SubMenu("accedi all'applicazione")
-menu_accesso_app.add_item(item_login)
-menu_accesso_app.add_item(item_registra)
+menu_accesso_app.add_item(_item_login)
+menu_accesso_app.add_item(_item_registra)
