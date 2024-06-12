ba37003243c12bbc79689b44e93c3f969f2611c2
taaazzi <gabrieleale2106@gmail.com>
2024-06-12 10:47:48 +0200

    inserito il menu x ubicazione

--- src/menu/menu_app.py
+++ src/menu/menu_app.py
@@ -2,15 +2,33 @@ from .menu import MenuItem, SubMenu, Cmd
 from view.registrazione_utente import ottieni_dati as get_reg_dati 
 from view.login_utente import ottieni_dati as get_log_dati
 from view.lista_eventi import eventi
+from view.input_ubicazione import menu, modifica_dati, inserimento_dati, elimina_dati
 from controller.user import UserRole
 
+# Creazione del menù ubicazione
+_cmd_inserisci_ubicazione = Cmd("Inserisci Ubicazione",inserimento_dati)
+_cmd_modifica_ubicazione = Cmd("Modifica Ubicazione",modifica_dati)
+_cmd_elimina_ubicazione = Cmd("Elimina Ubicazione",elimina_dati)
+
+_item_inserisci_ubicazione = MenuItem("Aggiungi Ubicazione",_cmd_inserisci_ubicazione,{UserRole.ADMIN, UserRole.VEND})
+_item_modifica_ubicazione = MenuItem("Modifica Ubicazione",_cmd_modifica_ubicazione,{UserRole.ADMIN, UserRole.VEND})
+_item_elimina_ubicazione = MenuItem("Elimina Ubicazione",_cmd_elimina_ubicazione,{UserRole.ADMIN, UserRole.VEND})
+
+menu_ubicazione = SubMenu("menù di ubicazione")
+menu_ubicazione.add_item(_item_inserisci_ubicazione)
+menu_ubicazione.add_item(_item_modifica_ubicazione)
+menu_ubicazione.add_item(_item_elimina_ubicazione)
+
 # Creazione del menù principale
-_cmd_menu_principale = Cmd("Menù principale", eventi)
+_cmd_menu_principale = Cmd("Lista Eventi", eventi)
+_cmd_ubicazione = Cmd("Ubicazione",menu,next_menu = menu_ubicazione)
 
 _item_menu_principale = MenuItem("Lista Eventi", _cmd_menu_principale, {UserRole.ADMIN, UserRole.VEND, UserRole.CLIENTE})
+_item_ubicazione = MenuItem("Gestisci Ubicazioni", _cmd_ubicazione, {UserRole.ADMIN, UserRole.VEND})
 
 menu_principale_app = SubMenu("menù principale dell'applicazione")
 menu_principale_app.add_item(_item_menu_principale)
+menu_principale_app.add_item(_item_ubicazione)
 
 # Inizializziamo il menù di accesso all'applicazione
 _cmd_registra = Cmd("Registrazione", get_reg_dati)
