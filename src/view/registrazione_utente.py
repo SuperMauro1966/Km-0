726e939bdf016531277b2b3d1abc19a02a78a6fe
taaazzi <gabrieleale2106@gmail.com>
2024-06-13 09:18:53 +0200

    eliminato e spostato l'input nel flie utils

--- src/view/registrazione_utente.py
+++ src/view/registrazione_utente.py
@@ -1,13 +1,12 @@
-from collections import namedtuple
 from controller import registrazione
+from .input_utils import InputField, input_dati
 # verificare la pwd e l'email.
 
-_InputField = namedtuple('_InputField', ['nome', 'etichetta', 'default', 'conv_func'])
 _param_idCredential = [
-    _InputField ('email', 'email', None, None), 
-    _InputField ('password', 'password', None, None),
-    _InputField ('codice_fiscale', 'codice fiscale', None, None),
-    _InputField ('telefono', 'numero di telefono', None, None)
+    InputField ('email', 'email', None, None), 
+    InputField ('password', 'password', None, None),
+    InputField ('codice_fiscale', 'codice fiscale', None, None),
+    InputField ('telefono', 'numero di telefono', None, None)
 ]
 
 def ottieni_dati() -> None:
@@ -28,7 +27,7 @@ def ottieni_dati() -> None:
     partitaIVA: partita IVA
     """
 
-    dati_cliente_venditore = _input_dati(_param_idCredential)
+    dati_cliente_venditore = input_dati(_param_idCredential)
 
     conf_psw = input("Digita un'altra volta la password per confermarla: ")
     while conf_psw != dati_cliente_venditore['password']:
@@ -44,48 +43,21 @@ def ottieni_dati() -> None:
     
     if dati_cliente_venditore['ruolo'].upper() == 'C':
         client_param = [
-            _InputField ('nome', 'nome', None, None), 
-            _InputField ('cognome', 'cognome', None, None),
-            _InputField ('citta', 'città', None, None),
-            _InputField ('provincia', 'provincia', None, None),
-            _InputField ('via', 'indirizzo di casa', None, None)
+            InputField ('nome', 'nome', None, None), 
+            InputField ('cognome', 'cognome', None, None),
+            InputField ('citta', 'città', None, None),
+            InputField ('provincia', 'provincia', None, None),
+            InputField ('via', 'indirizzo di casa', None, None)
         ]
-        dati_aggiuntivi = _input_dati(client_param)
+        dati_aggiuntivi = input_dati(client_param)
     else:
         seller_param = [
-            _InputField ('ragione_sociale', 'ragione sociale', None, None),
-            _InputField ('sitoweb', 'sito web', None, None),
-            _InputField ('partitaIVA', 'partita IVA', None, None)
+            InputField ('ragione_sociale', 'ragione sociale', None, None),
+            InputField ('sitoweb', 'sito web', None, None),
+            InputField ('partitaIVA', 'partita IVA', None, None)
         ]
-        dati_aggiuntivi = _input_dati(seller_param)
+        dati_aggiuntivi = input_dati(seller_param)
     
     dati_cliente_venditore.update(dati_aggiuntivi)
     status, msg = registrazione.registrati(dati_cliente_venditore)
     print(msg)
-
-def _input_dati(fields: list[_InputField]) -> dict:
-    """
-    Salva gli input del dizionario.
-    """
-    reg_param = {}
-
-    for input_el in fields:
-        while True:
-            temp_val = input(f"{input_el.etichetta}(default: {input_el.default}): ")
-            if temp_val == '':
-                if input_el.default:
-                    reg_param[input_el.nome] = input_el.default
-                    break
-                else:
-                    print("è necessario specificare un valore")
-            if input_el.conv_func:
-                try:
-                    reg_param[input_el.nome] = input_el.conv_func(temp_val)
-                except ValueError:
-                    print("Errore nella conversione del tipo di dati")
-                else:
-                    break
-            else:
-                reg_param[input_el.nome] = temp_val
-                break
-    return reg_param
